52efb03750c009ff3d98936e9a8d65a42d05a77e
DamianL <0225228@up.edu.mx>
2021-05-19 11:24:39 -0500

    Añadir más colores y una brocha

--- games/buscaminas.py
+++ games/buscaminas.py
@@ -6,6 +6,10 @@ colors["white"] = (255,255,255)
 colors["red"] = (255, 42, 55)
 colors["blue"] = (0, 135, 255)
 colors["gray"] = (204, 204, 204)
+colors["black"] = (255, 255, 255)
+colors["yellow"] = (255, 233, 0)
+colors["purple"] = (163, 73, 164)
+colors["green"] = (66, 255, 51)
 
 size = 5
 
@@ -18,12 +22,12 @@ class Rect:
     
     def draw(self):
         pygame.draw.rect(self.screen,self.color,self.rect)
-        pygame.draw.rect(self.screen,colors["gray"],self.rect,2)
+        pygame.draw.rect(self.screen,colors["white"],self.rect,2)
     
     def colision(self,turn):
         if turn:
             if(self.rect.collidepoint(pygame.mouse.get_pos())):
-                self.color = colors["red"]
+                self.color = Brush.color
 
 class Map:
     def __init__(self):
@@ -57,7 +61,7 @@ class Map:
 
 class Brush:
     def __init__(self):
-        self.color = colors["white"]
+        self.color = colors["blue"]
 
 class paintura:
     def __init__(self, screen, i, j, color):
@@ -76,6 +80,7 @@ def main():
     map = Map()
     screen = map.createWindow()
     map.createMap(screen)
+    brush = Brush()
     running = True
     paint = False
     while running:
