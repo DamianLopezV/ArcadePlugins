fcc15a5420f5fd4aa0cfc7c2137262bc30a9dd18
DamianL <0225228@up.edu.mx>
2021-05-25 21:06:38 -0500

    Eliminado Buscaminas

--- games/paint.py
+++ games/paint.py
@@ -6,12 +6,12 @@ colors["white"] = (255,255,255)
 colors["red"] = (255, 42, 55)
 colors["blue"] = (0, 135, 255)
 colors["gray"] = (204, 204, 204)
-colors["black"] = (255, 255, 255)
+colors["black"] = (0, 0, 0)
 colors["yellow"] = (255, 233, 0)
 colors["purple"] = (163, 73, 164)
 colors["green"] = (66, 255, 51)
 
-size = 5
+size = 10
 
 class Rect:
     def __init__(self,screen,i,j):
@@ -24,16 +24,14 @@ class Rect:
         pygame.draw.rect(self.screen,self.color,self.rect)
         pygame.draw.rect(self.screen,colors["white"],self.rect,2)
     
-    def colision(self,turn):
+    def colision(self,turn,brush):
         if turn:
             if(self.rect.collidepoint(pygame.mouse.get_pos())):
-                self.color = Brush.color
+                self.color = brush.color
 
 class Map:
     def __init__(self):
-        self.map = [[""]*100 for i in range(0,100)]
-        for i in range(len(map)):
-            self.map.append
+        self.map = [[""]*100 for i in range(100)]
         self.mapColums = len(self.map)
         self.mapRows = len(self.map[0])
 
@@ -47,10 +45,10 @@ class Map:
             for j in range(0,self.mapRows):
                 self.map[i][j].draw()
     
-    def playTurn(self,turn):
+    def playTurn(self,turn,brush):
         for i in range(0,self.mapColums):
             for j in range(0,self.mapRows):
-                self.map[i][j].colision(turn)
+                self.map[i][j].colision(turn,brush)
 
     def createWindow(self):
             (width, height) = (size*self.mapColums+1, size*self.mapRows+1)
@@ -62,18 +60,26 @@ class Map:
 class Brush:
     def __init__(self):
         self.color = colors["blue"]
-
-class paintura:
-    def __init__(self, screen, i, j, color):
-        self.screen = screen
-        self.rect = pygame.Rect(j*size, i*size, size, size)
-        self.color = color
-
-
-    def colision(self,turn):
-        if(self.rect.collidepoint(pygame.mouse.get_pos())):
-            return self.color
         
+    def controls(self,event):
+        if event.key == pygame.K_1:
+            self.color = colors["blue"]
+        if event.key == pygame.K_2:
+            self.color = colors["red"]
+        if event.key == pygame.K_3:
+            self.color = colors["yellow"]
+        if event.key == pygame.K_4:
+            self.color = colors["purple"]
+        if event.key == pygame.K_5:
+            self.color = colors["green"]
+        if event.key == pygame.K_6:
+            self.color = colors["white"]
+        if event.key == pygame.K_7:
+            self.color = colors["black"]
+        if event.key == pygame.K_8:
+            self.color = colors["gray"]
+        
+
 
 def main():
     pygame.init()
@@ -93,6 +99,9 @@ def main():
                 paint = True
             if event.type == pygame.MOUSEBUTTONUP:
                 paint = False
-            map.playTurn(paint)
+            if event.type == pygame.KEYDOWN:
+                brush.controls(event)
+            
+            map.playTurn(paint,brush)
 
 main()
